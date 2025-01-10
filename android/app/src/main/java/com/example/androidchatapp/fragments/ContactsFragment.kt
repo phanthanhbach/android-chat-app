ba721d2a8c124311d996cb7f8cedddc0eff03a67
Phan Thanh Bách <phanbach02@gmail.com>
2025-01-10 18:11:48 +0700

    Create ContactsFragment.kt

---
+++ android/app/src/main/java/com/example/androidchatapp/fragments/ContactsFragment.kt
@@ -0,0 +1,22 @@
+package com.example.androidchatapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.androidchatapp.R
+
+class ContactsFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_contacts, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
