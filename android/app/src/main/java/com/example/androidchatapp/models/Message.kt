0dc716eb8c572b60319a509314c1afaded83ab85
Phan Thanh Bách <phanbach02@gmail.com>
2025-01-14 12:33:56 +0700

    [Refactor] Change data type

--- android/app/src/main/java/com/example/androidchatapp/models/Message.kt
+++ android/app/src/main/java/com/example/androidchatapp/models/Message.kt
@@ -1,9 +1,11 @@
 package com.example.androidchatapp.models
 
+import java.util.Date
+
 class Message {
     var _id: String = ""
     var sender: User = User()
     var receiver: User = User()
     var message: String = ""
-    var createdAt: Double = 0.0
+    var createdAt: String = ""
 }
