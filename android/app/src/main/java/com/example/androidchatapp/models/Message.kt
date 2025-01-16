469a57dab8b4cc95fb2c9aef36c64e4f38ec1df6
Phan Thanh Bách <phanbach02@gmail.com>
2025-01-16 18:42:23 +0700

    [Feat] Change structure of Message class

--- android/app/src/main/java/com/example/androidchatapp/models/Message.kt
+++ android/app/src/main/java/com/example/androidchatapp/models/Message.kt
@@ -1,11 +1,12 @@
 package com.example.androidchatapp.models
 
-import java.util.Date
-
 class Message {
     var _id: String = ""
     var sender: User = User()
     var receiver: User = User()
     var message: String = ""
+    var attachment: String = ""
+    var attachmentName: String = ""
+    var extension: String = ""
     var createdAt: String = ""
 }
