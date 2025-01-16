d9d8c203b063f8af2cdfa844f6ea78eedd7026ef
Phan Thanh Bách <phanbach02@gmail.com>
2025-01-16 18:45:52 +0700

    [Feat] Add get file name and extension of img

--- android/app/src/main/java/com/example/androidchatapp/utils/Utility.kt
+++ android/app/src/main/java/com/example/androidchatapp/utils/Utility.kt
@@ -1,10 +1,42 @@
 package com.example.androidchatapp.utils
 
 import android.app.AlertDialog
+import android.content.ContentResolver
 import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.provider.OpenableColumns
+import android.webkit.MimeTypeMap
+import java.io.File
 
 object Utility {
-    var apiUrl: String = "http://192.168.1.4:3000"
+    var apiUrl: String = "http://172.16.0.2:3000"
+
+    fun getFileName(contentResolver: ContentResolver, uri: Uri): String {
+        val cursor: Cursor? = contentResolver.query(uri, null, null, null, null)
+        val nameIndex = cursor?.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+        cursor?.moveToFirst()
+        val fileName = nameIndex?.let {
+            cursor.getString(it)
+        }
+        cursor?.close()
+        return fileName.toString()
+    }
+
+    fun getExtension(contentResolver: ContentResolver, uri: Uri): String {
+        if (uri.scheme == ContentResolver.SCHEME_CONTENT) {
+            val mime = MimeTypeMap.getSingleton()
+            return mime.getExtensionFromMimeType(contentResolver.getType(uri)).toString()
+        } else {
+            return MimeTypeMap.getFileExtensionFromUrl(
+                Uri.fromFile(
+                    uri.path?.let {
+                        File(it)
+                    }
+                ).toString()
+            )
+        }
+    }
 
     fun showAlert(
         context: Context,
