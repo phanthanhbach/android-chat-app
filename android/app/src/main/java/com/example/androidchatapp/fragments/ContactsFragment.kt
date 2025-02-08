a04921fa22aea068ed0da691ac89f251cbeca848
Phan Thanh Bách <phanbach02@gmail.com>
2025-02-08 19:47:11 +0700

    [Feat] Add contact screen

--- android/app/src/main/java/com/example/androidchatapp/fragments/ContactsFragment.kt
+++ android/app/src/main/java/com/example/androidchatapp/fragments/ContactsFragment.kt
@@ -5,9 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.androidchatapp.R
+import com.example.androidchatapp.adapters.ContactsAdapter
+import com.example.androidchatapp.models.FetchContactsModel
+import com.example.androidchatapp.models.User
+import com.example.androidchatapp.utils.MySharedPreference
+import com.example.androidchatapp.utils.Utility
+import com.google.gson.Gson
+import com.example.androidchatapp.interfaces.RVInterface
 
-class ContactsFragment: Fragment() {
+
+class ContactsFragment: Fragment(), RVInterface {
+
+    lateinit var sharedPreference: MySharedPreference
+    lateinit var contacts: ArrayList<User>
+    lateinit var rv: RecyclerView
+    lateinit var adapter: ContactsAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,5 +37,56 @@ class ContactsFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        rv = view.findViewById(R.id.rv)
+        rv.layoutManager = LinearLayoutManager(context)
+
+        adapter = ContactsAdapter(ArrayList(), this)
+        rv.adapter = adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        contacts = ArrayList()
+        sharedPreference = MySharedPreference()
+        getData()
+    }
+
+    fun getData() {
+        val queue = Volley.newRequestQueue(context)
+        val url: String = Utility.apiUrl + "/contacts/fetch"
+
+        val stringRequest: StringRequest = object : StringRequest(
+            Method.POST,
+            url,
+            Response.Listener { response ->
+                val fetchContactsModel: FetchContactsModel =
+                    Gson().fromJson(response, FetchContactsModel::class.java)
+                if (fetchContactsModel.status == "success") {
+                    contacts = fetchContactsModel.contacts
+                    adapter.setData(contacts)
+                } else {
+                    context?.let {
+                        Utility.showAlert(it, "Error", fetchContactsModel.message)
+                    }
+                }
+            },
+            Response.ErrorListener { error -> }) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers: HashMap<String, String> = HashMap()
+                headers["Authorization"] =
+                    "Bearer " + context?.let {
+                        sharedPreference.getAccessToken(it)
+                    }
+
+                return headers
+            }
+        }
+        queue.add(stringRequest)
+    }
+
+    override fun onClick(view: View) {
+        TODO("Not yet implemented")
     }
 }
